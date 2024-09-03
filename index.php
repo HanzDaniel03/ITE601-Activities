c094e320900beb7acb163953562460a5149f41a3
BU�I <23010684@comlabserver.csabcomlab.edu.ph>
2024-09-03 18:42:05 +0800

    Payroll

--- index.php
+++ index.php
@@ -1,8 +1,36 @@
 <?php
 
-echo "hello world!"
-echo "gwapo ko!
-e"cho "OO eh”
-try changes
+ class Payroll{ 
+
+ 
+
+
+    private string $fn = "John";
+    private string $ln = "Volta";
+  
+
+    private float $tax = 50;
+    private float $salary = 500;
+
+public function computeNet(): float {
+   return $net = $this->salary - $this->tax;
+}
+
+    public function getEmployeeName(): string {
+     return  $Employee  = $this->fn. ", " .$this->ln;
+
+
+
+}
+    }
+
+
+$payroll = new payroll();
+
+echo "Employee Name: ". $payroll->getEmployeeName() . "<br/>";
+    echo "Net pay: " . $payroll->computeNet()
+
+
+
+
 ?>
-\html	
